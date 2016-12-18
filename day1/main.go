984dfaff9c1e6cfd1f6b2bb0e27bbdc402124f11
Bruce Downs <bruce.downs@jivesoftware.com>
2016-12-18 10:21:31 -0800

    day 1 of 2016

--- day1/main.go
+++ day1/main.go
@@ -4,33 +4,135 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
+	"strconv"
+	"strings"
 )
+
+const (
+	north = iota
+	east
+	south
+	west
+)
+
+type coordinate struct {
+	set  bool
+	x, y int
+}
+
+func (c coordinate) String() string {
+	return fmt.Sprintf("(%d, %d)", c.x, c.y)
+}
+
+func (c *coordinate) setIfUnset(u coordinate) {
+	//log.Printf("intersection %s", u)
+	if !c.set {
+		c.set = true
+		c.x = u.x
+		c.y = u.y
+	}
+}
+
+func (c *coordinate) distance() int {
+	return int(math.Abs(float64(c.x)) + math.Abs(float64(c.y)))
+}
+
+func visit(current coordinate,
+	firstIntersection *coordinate,
+	visited map[coordinate]struct{}) {
+	if _, ok := visited[current]; ok {
+		(*firstIntersection).setIfUnset(current)
+	} else {
+		visited[current] = struct{}{}
+	}
+}
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		var flight int
-		var basementPosition int
+		direction := north
+		var current, firstIntersection coordinate
+		visited := make(map[coordinate]struct{})
 
-		for k, v := range line {
-			switch v {
-			case '(':
-				flight++
-			case ')':
-				flight--
-			default:
-				log.Fatalf("invalid input [%s]", string(v))
+		for _, v := range strings.Split(line, ",") {
+			token := strings.TrimSpace(v)
+			//log.Printf("element %s", token)
+			if len(token) < 2 {
+				log.Fatalf("Invalid input: %s", line)
 			}
 
-			if basementPosition < 1 && flight < 0 {
-				basementPosition = k + 1
+			distance, err := strconv.Atoi(token[1:])
+			if err != nil {
+				log.Fatalf("Invalid input: %s", token)
+			}
+
+			switch token[0] {
+			case 'R':
+				//log.Printf("Turn right and move %d", distance)
+				switch direction {
+				case north:
+					direction = east
+				case east:
+					direction = south
+				case south:
+					direction = west
+				case west:
+					direction = north
+				default:
+					log.Fatal("Invalid logic")
+				}
+
+			case 'L':
+				//log.Printf("Turn left and move %d", distance)
+				switch direction {
+				case north:
+					direction = west
+				case east:
+					direction = north
+				case south:
+					direction = east
+				case west:
+					direction = south
+				default:
+					log.Fatal("Invalid logic")
+				}
+			default:
+				log.Fatalf("Invalid input: %s", token)
+			}
+
+			switch direction {
+			case north:
+				for i := 0; i < distance; i++ {
+					current.y++
+					visit(current, &firstIntersection, visited)
+				}
+			case east:
+				for i := 0; i < distance; i++ {
+					current.x++
+					visit(current, &firstIntersection, visited)
+				}
+			case west:
+				for i := 0; i < distance; i++ {
+					current.x--
+					visit(current, &firstIntersection, visited)
+				}
+			case south:
+				for i := 0; i < distance; i++ {
+					current.y--
+					visit(current, &firstIntersection, visited)
+				}
+			default:
+				log.Fatalf("Invalid logic %d", direction)
 			}
 		}
 
-		fmt.Printf("current floor is %d\n", flight)
-		fmt.Printf("entered basement at position %d\n", basementPosition)
+		log.Printf("End coordinates %s - total block: %d",
+			current, current.distance())
+		log.Printf("First intersection %s - total blocks: %d",
+			firstIntersection, firstIntersection.distance())
 	}
 }
