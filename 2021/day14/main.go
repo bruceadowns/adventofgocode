47881047962686b16115471640ed047b9649a214
Bruce Downs <bruce.downs@autodesk.com>
2021-12-17 12:45:37 -0800

    advent of code part 2 of day 14 of 2021

--- 2021/day14/main.go
+++ 2021/day14/main.go
@@ -9,11 +9,15 @@ import (
 	"os"
 )
 
-type rule [2]uint8
+type pair [2]uint8
+
+func (p pair) String() string {
+	return fmt.Sprintf("%c%c", p[0], p[1])
+}
 
 type Manual struct {
 	template []uint8
-	rules    map[rule]uint8
+	rules    map[pair]uint8
 }
 
 func Part1(in Manual) (res int) {
@@ -24,7 +28,7 @@ func Part1(in Manual) (res int) {
 		for j := 0; j < len(polymer)-1; j++ {
 			newPolymer = append(newPolymer, polymer[j])
 
-			if v, exists := in.rules[rule{polymer[j], polymer[j+1]}]; exists {
+			if v, exists := in.rules[pair{polymer[j], polymer[j+1]}]; exists {
 				newPolymer = append(newPolymer, v)
 			}
 		}
@@ -51,11 +55,52 @@ func Part1(in Manual) (res int) {
 }
 
 func Part2(in Manual) (res int) {
-	return
+	pairs := make(map[pair]int)
+	for i := 0; i < len(in.template)-1; i++ {
+		pairs[pair{in.template[i], in.template[i+1]}]++
+	}
+
+	for i := 0; i < 40; i++ {
+		newPairs := make(map[pair]int)
+		for k, v := range pairs {
+			newPairs[k] = v
+		}
+
+		for k, v := range pairs {
+			if vv, exists := in.rules[k]; exists {
+				newPairs[k] -= v
+				newPairs[pair{k[0], vv}] += v
+				newPairs[pair{vv, k[1]}] += v
+			}
+		}
+
+		pairs = newPairs
+	}
+
+	counts := make(map[uint8]int)
+	for k, v := range pairs {
+		counts[k[0]] += v
+		counts[k[1]] += v
+	}
+	counts[in.template[0]]++
+	counts[in.template[len(in.template)-1]]++
+
+	min := math.MaxInt
+	max := 0
+	for _, v := range counts {
+		if v < min {
+			min = v
+		}
+		if v > max {
+			max = v
+		}
+	}
+
+	return max/2 - min/2
 }
 
 func In(r io.Reader) (res Manual) {
-	res.rules = make(map[rule]uint8)
+	res.rules = make(map[pair]uint8)
 
 	scanner := bufio.NewScanner(r)
 	if !scanner.Scan() {
@@ -68,25 +113,25 @@ func In(r io.Reader) (res Manual) {
 	}
 
 	for scanner.Scan() {
-		line := scanner.Text()
+		line = scanner.Text()
 		if len(line) == 0 {
 			continue
 		}
 
-		var pair string
-		var insert uint8
-		n, err := fmt.Sscanf(line, "%s -> %c", &pair, &insert)
+		var p string
+		var i uint8
+		n, err := fmt.Sscanf(line, "%s -> %c", &p, &i)
 		if err != nil {
 			log.Fatal("invalid input")
 		}
 		if n != 2 {
 			log.Fatal("invalid input")
 		}
-		if len(pair) != 2 {
+		if len(p) != 2 {
 			log.Fatal("invalid input")
 		}
 
-		res.rules[rule{pair[0], pair[1]}] = insert
+		res.rules[pair{p[0], p[1]}] = i
 	}
 
 	return
