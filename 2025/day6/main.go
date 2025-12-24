c1541bc7a5152cb8e9c644ea948c7f5226eb0808
Bruce Downs <bruce.downs@bluestaq.com>
2025-12-24 09:19:52 -0700

    advent of code part 2 of day 6 of 2025

--- 2025/day6/main.go
+++ 2025/day6/main.go
@@ -15,19 +15,31 @@ const (
 )
 
 type Equation struct {
-	args  []int
-	op    int
-	total int
+	args []int
+	op   int
 }
-type Input []Equation
+type Input1 []Equation
 
-func In(r io.Reader) (res Input) {
-	var lines []string
+type Card struct {
+	minX int
+	maxX int
+	op   int
+}
+type Input2 struct {
+	rlines []string
+	cards  []Card
+}
+
+func In(r io.Reader) (res []string) {
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+		res = append(res, scanner.Text())
 	}
 
+	return
+}
+
+func In1(lines []string) (res Input1) {
 	for range strings.Fields(lines[1]) {
 		res = append(res, Equation{})
 	}
@@ -37,7 +49,7 @@ func In(r io.Reader) (res Input) {
 		for k, v := range strings.Fields(lines[i]) {
 			j, err := strconv.Atoi(v)
 			if err != nil {
-				log.Fatal("invalid")
+				log.Fatal()
 			}
 
 			res[k].args = append(res[k].args, j)
@@ -48,43 +60,144 @@ func In(r io.Reader) (res Input) {
 		switch v {
 		case "+":
 			res[k].op = ADD
-
-			for _, vv := range res[k].args {
-				res[k].total += vv
-			}
 		case "*":
 			res[k].op = MULTIPLY
-
-			res[k].total = 1
-			for _, vv := range res[k].args {
-				res[k].total *= vv
-			}
 		default:
-			log.Fatal("invalid")
+			log.Fatal()
 		}
 	}
 
 	return
 }
 
-func Part1(in Input) (res int) {
-	for _, v := range in {
-		res += v.total
+func In2(lines []string) (res Input2) {
+	for i := len(lines) - 2; i >= 0; i-- {
+		res.rlines = append(res.rlines, lines[i])
+	}
+	ops := lines[len(lines)-1]
+
+	var indices []int
+	for k, v := range ops {
+		switch v {
+		case '+', '*':
+			indices = append(indices, k)
+		}
+	}
+	indices = append(indices, len(ops)+1)
+
+	for i := 0; i < len(indices)-1; i++ {
+		switch ops[indices[i]] {
+		case '+':
+			res.cards = append(res.cards, Card{indices[i], indices[i+1] - 2, ADD})
+		case '*':
+			res.cards = append(res.cards, Card{indices[i], indices[i+1] - 2, MULTIPLY})
+		}
 	}
 
 	return
 }
 
-func Part2(in Input) (res int) {
+func Part1(in Input1) (res int) {
 	for _, v := range in {
-		res += v.total
+		var total int
+		switch v.op {
+		case ADD:
+			for _, vv := range v.args {
+				total += vv
+			}
+		case MULTIPLY:
+			total = 1
+			for _, vv := range v.args {
+				total *= vv
+			}
+		default:
+			log.Fatal()
+		}
+
+		res += total
+	}
+
+	return
+}
+
+func Part2(in Input2) (res int) {
+	for _, v := range in.cards {
+		var nums []int
+		for x := v.maxX; x >= v.minX; x-- {
+			var num int
+			for y := len(in.rlines) - 1; y >= 0; y-- {
+				n := in.rlines[y][x]
+				switch n {
+				case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+					num = num*10 + int(n-'0')
+				}
+			}
+
+			nums = append(nums, num)
+		}
+
+		var total int
+		switch v.op {
+		case ADD:
+			for _, v := range nums {
+				total += v
+			}
+		case MULTIPLY:
+			total = 1
+			for _, v := range nums {
+				total *= v
+			}
+		}
+
+		res += total
+	}
+
+	return
+}
+
+func Part21(in Input2) (res int) {
+	for _, v := range in.cards {
+		var nums []int
+		for y := len(in.rlines) - 1; y >= 0; y-- {
+			var num int
+			for x := v.minX; x <= v.maxX; x++ {
+				n := in.rlines[y][x]
+				switch n {
+				case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+					num = num*10 + int(n-'0')
+				}
+			}
+
+			nums = append(nums, num)
+		}
+
+		var total int
+		switch v.op {
+		case ADD:
+			for _, v := range nums {
+				total += v
+			}
+		case MULTIPLY:
+			total = 1
+			for _, v := range nums {
+				total *= v
+			}
+		}
+
+		res += total
 	}
 
 	return
 }
 
 func main() {
-	i := In(os.Stdin)
-	log.Printf("part1: %d", Part1(i))
-	log.Printf("part2: %d", Part2(i))
+	lines := In(os.Stdin)
+
+	in1 := In1(lines)
+	log.Printf("part1: %d", Part1(in1))
+
+	in2 := In2(lines)
+	log.Printf("part2: %d", Part2(in2))
+
+	log.Printf("part21: %d", Part21(in2))
 }
--- 2025/day6/main_test.go
+++ 2025/day6/main_test.go
@@ -5,19 +5,24 @@ import (
 	"testing"
 )
 
-var lines Input
+var (
+	lines []string
+	in1   Input1
+	in2   Input2
+)
 
 func init() {
 	stdin := `123 328  51 64 
  45 64  387 23 
   6 98  215 314
-*   +   *   + 
+*   +   *   +  
 `
 	lines = In(bytes.NewBufferString(stdin))
 }
 
 func Test_Part1(t *testing.T) {
-	actual := Part1(lines)
+	in1 = In1(lines)
+	actual := Part1(in1)
 	expected := 4277556
 
 	if actual != expected {
@@ -26,8 +31,19 @@ func Test_Part1(t *testing.T) {
 }
 
 func Test_Part2(t *testing.T) {
-	actual := Part2(lines)
+	in2 = In2(lines)
+	actual := Part2(in2)
 	expected := 3263827
+
+	if actual != expected {
+		t.Errorf("actual: %d expected: %d", actual, expected)
+	}
+}
+
+func Test_Part21(t *testing.T) {
+	in2 = In2(lines)
+	actual := Part21(in2)
+	expected := 4277556
 
 	if actual != expected {
 		t.Errorf("actual: %d expected: %d", actual, expected)
